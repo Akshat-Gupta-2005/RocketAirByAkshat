0af71a08df48b159aa038fb008af4f7e2153bc92
AKSHAT GUPTA <gemini.akshatgupta@gmail.com>
2025-03-08 18:54:15 +0530

    Added Page 4

--- script.js
+++ script.js
@@ -22,18 +22,18 @@ function page1() {
         ease: "power2.in"
     })
 
-    tl.from("#page1 svg" , {
-        x:"-1000",
-        opacity:0,
-        duration:0.2,
-        ease:"power2.out"
-    } , "cool")
-    tl.from("#page2 " , {
-        y:"10",
-        opacity:0,
-        duration:0.2,
-        ease:"power2.out"
-    } , "cool")
+    tl.from("#page1 svg", {
+        x: "-1000",
+        opacity: 0,
+        duration: 0.2,
+        ease: "power2.out"
+    }, "cool")
+    tl.from("#page2 ", {
+        y: "10",
+        opacity: 0,
+        duration: 0.2,
+        ease: "power2.out"
+    }, "cool")
 
     tl.from("#page1 .load2", {
         y: 20,
@@ -83,42 +83,42 @@ function page1() {
     })
 
     let getintouch = document.getElementById("getintouch")
-    getintouch.addEventListener("mouseenter" , ()=>{
-        gsap.to("#getintouch #up" , {
-            y:-16,
-            duration:0.2,
+    getintouch.addEventListener("mouseenter", () => {
+        gsap.to("#getintouch #up", {
+            y: -16,
+            duration: 0.2,
         })
-        gsap.to("#getintouch #down" , {
-            y:16,
-            duration:0.2,
+        gsap.to("#getintouch #down", {
+            y: 16,
+            duration: 0.2,
         })
-        gsap.to("#getintouch #text" , {
-            color:"white",
+        gsap.to("#getintouch #text", {
+            color: "white",
             // duration:0.1,
         })
     })
-    getintouch.addEventListener("mouseleave" , ()=>{
-        gsap.to("#getintouch #up" , {
-            y:0,
-            duration:0.2,
+    getintouch.addEventListener("mouseleave", () => {
+        gsap.to("#getintouch #up", {
+            y: 0,
+            duration: 0.2,
         })
-        gsap.to("#getintouch #down" , {
-            y:0,
-            duration:0.2,
+        gsap.to("#getintouch #down", {
+            y: 0,
+            duration: 0.2,
         })
-        gsap.to("#getintouch #text" , {
-            color:"black",
+        gsap.to("#getintouch #text", {
+            color: "black",
             // duration:0.2,
         })
     })
 
     let fourdots = document.getElementById("fourdots")
-    fourdots.addEventListener("mouseenter",()=>{
+    fourdots.addEventListener("mouseenter", () => {
         document.querySelectorAll("#fourdots circle").forEach(circle => {
             circle.setAttribute("fill", "#dcfe01");
         });
     })
-    fourdots.addEventListener("mouseleave",()=>{
+    fourdots.addEventListener("mouseleave", () => {
         document.querySelectorAll("#fourdots circle").forEach(circle => {
             circle.setAttribute("fill", "white");
         });
@@ -146,7 +146,7 @@ function page2() {
 
     overlay2.addEventListener("mousemove", (dets) => {
         gsap.to("#cursorthis", {
-            x: dets.pageX  - 50,
+            x: dets.pageX - 50,
             y: dets.pageY - 600,
             duration: 0.1,
         })
@@ -175,5 +175,129 @@ function page2() {
     })
 }
 
+function page3() {
+    gsap.from("#midtext", {
+        opacity: 0,
+        y:20,
+        scrollTrigger: {
+            trigger: "#midtext",
+            scroller: "body",
+            // markers: true,
+            //------------------important--------------------
+            start:"top 40%",
+            // end: "top -100%",
+            // scrub: 1,
+            // pin: true,
+            // pinSpacing: false
+        }
+    })
+
+    gsap.from("#wecallittext", {
+        opacity: 0,
+        y:20,
+        scrollTrigger: {
+            trigger: "#wecallittext",
+            scroller: "body",
+            // markers: true,
+            //------------------important--------------------
+            start:"top 95%",
+            // end: "top -100%",
+            // scrub: 1,
+            // pin: true,
+            // pinSpacing: false
+        }
+    })
+
+    gsap.from("#wecallitsvg", {
+        opacity: 0,
+        x:"-100%",
+        scrollTrigger: {
+            trigger: "#wecallitsvg",
+            scroller: "body",
+            // markers: true,
+            //------------------important--------------------
+            start:"top 85%",
+            ease: "power2.out"
+            // end: "top -100%",
+            // scrub: 1,
+            // pin: true,
+            // pinSpacing: false
+        }
+    })
+
+    gsap.from("#orbitaldesigntext1", {
+        opacity: 0,
+        y:"50",
+        scrollTrigger: {
+            trigger: "#orbitaldesigntext1",
+            scroller: "body",
+            // markers: true,
+            //------------------important--------------------
+            start:"top 85%",
+            ease: "power2.out"
+            // end: "top -100%",
+            // scrub: 1,
+            // pin: true,
+            // pinSpacing: false
+        }
+    })
+    gsap.from("#orbitaldesigntext2 , #gettoknowus",{
+        opacity: 0,
+        y:"50",
+        scrollTrigger: {
+            trigger: "#orbitaldesigntext2",
+            scroller: "body",
+            // markers: true,
+            //------------------important--------------------
+            start:"top 85%",
+            ease: "power2.out"
+            // end: "top -100%",
+            // scrub: 1,
+            // pin: true,
+            // pinSpacing: false
+        }
+    })
+
+    let gettoknowus = document.getElementById("gettoknowus")
+    gettoknowus.addEventListener("mouseenter", () => {        
+        
+        gsap.to("#gettoknowus #up", {
+            y: "-100%",
+            duration: 0.2,
+        })
+        gsap.to("#gettoknowus #down", {
+            y: "100%",
+            duration: 0.2,
+        })
+        gsap.to("#gettoknowus #text", {
+            color: "black",
+            // duration:0.1,
+        })
+    })
+    gettoknowus.addEventListener("mouseleave", () => {
+        gsap.to("#gettoknowus #up", {
+            y: 0,
+            duration: 0.2,
+        })
+        gsap.to("#gettoknowus #down", {
+            y: 0,
+            duration: 0.2,
+        })
+        gsap.to("#gettoknowus #text", {
+            color: "white",
+            // duration:0.2,
+        })
+    })
+}
+
 page1()
 page2()
+page3()
+
+// document.addEventListener("wheel", function(event) {
+//     event.preventDefault();
+//     window.scrollBy({
+//         top: event.deltaY * 1, // Adjust speed (lower = slower, higher = faster)
+//         behavior: "smooth"
+//     });
+// }, { passive: false });
